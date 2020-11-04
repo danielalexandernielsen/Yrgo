66366abc5b3aafa2bde3f4f8ebaeb41c9dcf587c
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-11-04 11:28:17 +0100

    Lesson 17

---
+++ Daniel_17_UIandSceneManagement/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+	public static GameSceneManager instance;  //Singleton instance
+
+	// public GameState currentState;  //State Machine state.
+
+	void Start()
+	{
+		if (instance == null)
+        {
+			instance = this;	//Save our object so we can use it easily
+			DontDestroyOnLoad(gameObject);
+        }
+		else
+        {
+			Destroy(gameObject);   //If we already have an instance, avoid creating another.
+        }
+	}
+
+	public void ChangeScene(string name)
+    {
+		SceneManager.LoadScene(name);
+    }
+
+	public void ReloadCurrentScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void LoadNextScene()
+    {
+		int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+		nextIndex = nextIndex % SceneManager.sceneCount;
+		SceneManager.LoadScene(nextIndex);
+	}
+
+	public void LoadPreviousScene()
+	{
+		int nextIndex = SceneManager.GetActiveScene().buildIndex - 1 + SceneManager.sceneCount;
+		nextIndex = nextIndex % SceneManager.sceneCount;
+		SceneManager.LoadScene(nextIndex);
+	}
+
+    private void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.B))
+        {
+			GameSceneManager.instance.LoadPreviousScene();
+        }
+
+		if (Input.GetKeyDown(KeyCode.N))
+		{
+			GameSceneManager.instance.LoadNextScene();
+		}
+	}
+
+}
---
+++ Daniel_17_UIandSceneManagement/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public Text bulletText;
+    public TextMeshProUGUI bulletTMPText;
+    public GameObject bullet;
+    public GameObject gun;
+
+    int numberOfBullets = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject newBullet = Instantiate(bullet, gun.transform.position, gun.transform.rotation);
+            newBullet.GetComponent<Rigidbody2D>().velocity = gun.transform.right * 10;
+
+            numberOfBullets++;
+            bulletText.text = "Bullets fired: " + numberOfBullets;
+            bulletTMPText.text = "Bullets fired: " + numberOfBullets;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GameSceneManager.instance.ChangeScene("Menu");
+        }
+    }
+}
