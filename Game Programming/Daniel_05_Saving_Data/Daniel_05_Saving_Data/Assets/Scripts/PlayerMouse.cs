45eac295937c15155f9b2f7d486a7e7e97a72bb9
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2021-01-22 11:02:01 +0100

    Saving data

---
+++ Game Programming/Daniel_05_Saving_Data/Daniel_05_Saving_Data/Assets/Scripts/PlayerMouse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouse : MonoBehaviour
+{
+
+    PlayerMovement pm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouse = Input.mousePosition;
+            move = mouse - Camera.main.WorldToScreenPoint(transform.position);
+        }
+
+        pm.MovePlayer(move);
+    }
+}
