52b54013bc0e7045307f67dac1fa1a53800201a2
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2021-03-06 21:30:57 +0100

    Tweaks

---
+++ Game Programming/CockroachFootNuclearBomb/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+}
